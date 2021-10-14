c98f64d52dbe5bb1e98070600401d68ff8e4f1af
Miguel A. Peña <miapenahu@unal.edu.co>
2021-10-13 21:30:03 -0500

    Reto 0 - Hello World in React Native added

---
+++ Reto 0/HelloWorld/App.js
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Image
+          style={{width: 150, height: 150 }}
+          source={ {uri: 'https://picsum.photos/200'} }
+        />
+      <Text>¡Hola Mundo!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
